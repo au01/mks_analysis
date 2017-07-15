03b4d75103373a41a05c10d814074f36fe69abee
auag92 <apaar92@gmail.com>
2017-07-14 21:46:41 -0400

    clean up done

--- testbed/hello.py
+++
@@ -1,5 +0,0 @@
-def hello_world(my_name):
-	print("Hello World, this is %s" % my_name)
-
-if __name__ == "__main__":
-	hello_world("Apaar Shanker")
--- testbed/mks_localisation.py
+++
@@ -1,4 +0,0 @@
-from .filter import Filter
-from scipy.linalg import lstsq
-from sklearn.linear_model import LinearRegression
-import numpy as np
